d526599c635624b4f80498559ab26c0916e40717
Uday1720 <udaykiranr03@gmail.com>
2022-11-10 22:57:50 -0600

    Updated the Part 2 code

--- controllers/film.js
+++ controllers/film.js
@@ -47,9 +47,26 @@ exports.film_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: Film delete DELETE ' + req.params.id);
 };
 // Handle Film update form on PUT.
-exports.film_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Film update PUT' + req.params.id);
-};
+exports.film_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Film.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.name)  
+               toUpdate.name = req.body.name; 
+    if(req.body.director) toUpdate.director = req.body.director; 
+        if(req.body.duration) toUpdate.duration = req.body.duration; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+    } 
+}; 
+
+
 exports.flim_view_all_Page = async function (req, res) {
     try {
         theFilms = await Film.find();
