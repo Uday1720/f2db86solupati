7a2b0a69f954a924e1ee6d80b87f3c7f26c26050
Uday1720 <udaykiranr03@gmail.com>
2022-11-29 00:40:30 -0600

    Passport assignment added validations to schema

--- models/film.js
+++ models/film.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose")
 const filmSchema = mongoose.Schema({
-name: String,
-director: String,
-duration: Number
+name: {
+    type: String,
+    required: [true, "Film Name cannot be empty"]
+},
+director: {
+    type: String,
+    required: [true, "Director Name cannot be empty"]
+},
+duration: {
+    type: Number,
+    required: [true, "Duration cannot be empty"],
+    min: 0,
+    max: 180
+}
 })
 module.exports = mongoose.model("Film",
 filmSchema)
