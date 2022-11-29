98032a34cb16eac7bcba08d3224f439d214c22b4
Uday1720 <udaykiranr03@gmail.com>
2022-11-29 00:34:34 -0600

    Passport Assignment added login check to update

--- routes/films.js
+++ routes/films.js
@@ -1,6 +1,18 @@
 var express = require('express');
 const film_controlers = require('../controllers/film');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
+
 /* GET films */
 router.get('/', film_controlers.flim_view_all_Page);
 
@@ -11,7 +23,7 @@ router.get('/detail', film_controlers.film_view_one_Page);
 router.get('/create', film_controlers.film_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', film_controlers.film_update_Page); 
+router.get('/update', secured, film_controlers.film_update_Page); 
 
 /* GET delete film page */ 
 router.get('/delete', film_controlers.film_delete_Page);
